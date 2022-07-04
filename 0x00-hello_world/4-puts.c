031f6264398b7066f1b23cf08641d8ded44f8069
developerprojectteams <marvebirthday7@gmail.com.helper>
2022-07-03 22:46:43 -0700

    2-assembler

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <sring.h>
+/**
+ * main -main block
+ * Return: o
+ */
+
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
