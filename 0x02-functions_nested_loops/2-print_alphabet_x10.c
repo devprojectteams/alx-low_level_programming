692078c66e752bfc6956ba62cc4542306f5f3a4a
developerprojectteams <marvebirthday7@gmail.com.helper>
2022-07-06 08:20:51 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 
 /**
 * print_alphabet_x10 - print alphabet 10 times*
@@ -14,9 +14,9 @@ while (round < 10)
 letter = 'a';
 while (letter <= 'z')
 {
-_putchar(letter);														
-letter++;															
-}								
+_putchar(letter);
+letter++;
+}
 _putchar('\n');
 round++;
 }
