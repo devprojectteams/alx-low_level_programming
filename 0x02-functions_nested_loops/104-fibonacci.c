3c0765b73c5ded7d7d6e14ab27e4cd1640ee5f29
developerprojectteams <marvebirthday7@gmail.com.helper>
2022-07-06 20:02:17 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,33 +1,40 @@
+#include <stdio.h>
+
 /**
-* main - finds and prints the first 98 Fibonacci numbers,
-* starting with 1 and 2
-* followed by a new line
-* Return: ALways 0 (Success)
+* main - print first 98 Fibonacci numbers without using long long, malloc,
+* pointers, array/tables, or structures
+* Return: 0
 */
+
 int main(void)
 {
-unsigned long int i, j, k, j1, j2, k1, k2;
-j = 1;
-k = 2;
-printf("%lu", j);
-for (i = 1; i < 91; i++)
+int counter, overflow;
+unsigned long a = 1;
+unsigned long b = 1;
+unsigned long sum = 0;
+long a_head, a_tail, b_head, b_tail, sum_head, sum_tail;
+printf("1");
+for (counter = 2; counter < 93; counter++) /* will still print */
 {
-printf(", %lu", k);
-k = k + j;
-j = k - j;
+sum = a + b;
+a = b;
+b = sum;
+printf(", %lu", sum);
 }
-j1 = j / 1000000000;
-j2 = j % 1000000000;
-k1 = k / 1000000000;
-k2 = k % 1000000000;
-for (i = 92; i < 99; ++i)
+a_head = a / 1000000000; /* break larger num into 2 parts */
+a_tail = a % 1000000000;
+b_head = b / 1000000000;
+b_tail = b % 1000000000;
+for (; counter < 99; counter++)
 {
-printf(", %lu", k1 + (k2 / 1000000000));
-printf("%lu", k2 % 1000000000);
-k1 = k1 + j1;
-j1 = k1 - j1;
-k2 = k2 + j2;
-j2 = k2 - j2;
+overflow = (a_tail + b_tail) / 1000000000;
+sum_tail = (a_tail + b_tail)-(1000000000 * overflow);
+sum_head = (a_head + b_head)+overflow;
+printf(", %lu%lu", sum_head, sum_tail);
+a_head = b_head;
+a_tail = b_tail;
+b_head = sum_head;
+b_tail = sum_tail;
 }
 printf("\n");
 return (0);
