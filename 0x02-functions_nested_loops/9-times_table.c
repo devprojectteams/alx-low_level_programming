0f739df6fee34a260a87038dd79b8c7d1f2e44ce
developerprojectteams <marvebirthday7@gmail.com.helper>
2022-07-06 18:33:05 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,7 +4,7 @@
 * times_table - prints the 9 times table
 * Description: prints the 9 times table
 * Return: void
- * */
+*/
 
 void times_table(void)
 {
@@ -13,15 +13,18 @@ for (row = 0; row <= 9; row++)
 {
 for (column = 0; column <= 9; column++)
 {
-product = row * column;					tens = product / 10;
+product = row * column;
+tens = product / 10;
 ones = product % 10;
 {
 if (column == 0)
 {
 _putchar('0');
 }
-else if (product < 10)						{
-_putchar(',');							_putchar(' ');
+else if (product < 10)
+{
+_putchar(',');
+_putchar(' ');
 _putchar(' ');
 _putchar(ones + '0');
 }
