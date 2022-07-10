bb8e7a5cd223fe156c634b6555ae8de7eaddf31b
developerprojectteams <marvebirthday7@gmail.com.helper>
2022-07-10 05:11:26 -0700

    task updated

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,7 +11,6 @@ int var;
 for (var = 0; var < 99; var++)
 {
 int ii;
-			
 for (ii = var + 1; ii < 100; ii++)
 {
 putchar((var / 10) + '0');
@@ -19,7 +18,6 @@ putchar((var % 10) + '0');
 putchar(' ');
 putchar((ii / 10) + '0');
 putchar((ii % 10) + '0');
-					
 if (var != 98)
 {
 putchar(',');
