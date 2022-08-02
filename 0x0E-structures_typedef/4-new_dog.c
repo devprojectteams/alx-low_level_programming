fecfbcc3aef790f5769a367ee5bdde8ac9aff5c6
developerprojectteams <marvebirthday7@gmail.com.helper>
2022-08-01 21:18:29 -0700

    hot fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 /**
-* new_dog - create a new dog
-* @name: char string name
-* @age: int age
-* @owner: char string owner
-* Return: pointer to new dog
-*/
+ * * new_dog - create a new dog
+ * * @name: char string name
+ * * @age: int age
+ * * @owner: char string owner
+ * * Return: pointer to new dog
+ * */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 struct dog *doge;
@@ -21,7 +21,7 @@ return (NULL);
 for (i = 0; name[i] != '\0'; i++)
 		;
 for (j = 0; owner[j] != '\0'; j++)
-		;
+;
 
 n = malloc(sizeof(char) * i + 1);
 if (n == NULL)
